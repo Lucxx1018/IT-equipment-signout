0cee5ecc89a0077b7182f0b054e59ea4388f2a97
Lucxx1018 <lucyazure2845@gmail.com>
2023-10-02 10:07:04 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, redirect, url_for, request, session, g
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, FileField, DateTimeField
+from wtforms.validators import DataRequired
+import sqlite3
+import random
+import string
+import datetime
+
+
+class NameForm(FlaskForm):
+    submit = SubmitField("Click here when all fields are filled in.")
+
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "please remember to set a password here later"
