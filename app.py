76cf3562c6a1a934cfba8943c71a43c0a6e99b48
Lucxx1018 <lucyazure2845@gmail.com>
2023-10-02 13:33:59 +0100

    Implements SQL, handles dates better

--- app.py
+++ app.py
@@ -1,21 +1,18 @@
-from flask import Flask, render_template, redirect, url_for, request, session, g
+from flask import Flask, render_template, g
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, FileField, DateTimeField, SelectField
+from wtforms import StringField, SubmitField, FileField, SelectField
 from wtforms.validators import DataRequired
 import sqlite3
-import random
 import string
 import datetime
 
 
 class NameForm(FlaskForm):
     name = StringField("Put your name here:", validators=[DataRequired()])
-    date = DateTimeField("Current date:")
-    time = DateTimeField("Current time:")
     equipment = SelectField(
         "Select equipment",
         choices=[("test", "test"), ("but with different text oooo", "also test")],
-        validators=DataRequired(),
+        validators=[DataRequired()],
     )
     signature = FileField("Attach the file with the signature here:", validators=[DataRequired()])
     submit = SubmitField("Click here when all fields are filled in.")
@@ -23,3 +20,35 @@ class NameForm(FlaskForm):
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "please remember to set a password here later"
+
+
+def get_db():
+    if getattr(g, "_database", None) is None:
+        g._database = sqlite3.connect("database.db")
+
+    return g._database
+
+
+@app.teardown_appcontext
+def close_db(*args):
+    db = getattr(g, "_database", None)
+    if db is not None:
+        db.close()
+
+
+with open("schema.sql") as file, app.app_context():
+    get_db().executescript(file.read())
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    form = NameForm()
+    if form.validate_on_submit():
+        cursor = get_db().cursor()
+        date = datetime.date.today().strftime("%Y-%M-%D")
+        time = datetime.datetime.now().strftime("%H:%M:%S")
+        cursor.execute(
+            "INSERT INTO equipment_log() VALUES($1, $2, $3, $4, $5) ",
+            (form.name.data, date, time, form.equipment.data, NotImplemented),
+        )
+    return render_template("index.jinja", form=form)
--- schema.sql
+++ schema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS equipment_log(
     name TEXT,
-    date TEXT PRIMARY KEY,
-    time TEXT,
+    date DATE PRIMARY KEY,
+    time TIME,
     equipment TEXT,
     signature TEXT
 )
