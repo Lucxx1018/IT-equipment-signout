56656973ff683ec5d3ffbe4d35490034c5e942b3
Lucy <lucyazure2845@gmail.com>
2023-10-16 09:26:49 +0100

    Add temporary second page, remove some placeholders

--- app.py
+++ app.py
@@ -30,19 +30,19 @@ with open("schema.sql") as file, app.app_context():
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    if request.json is not None:
-        response_encoded: str = request.json["dataUri"].split(",")[1]
-        response = base64.b64decode(response_encoded)
-        if True:  # TODO: Check if signature already exists before making it again
-            with open("response.svg", "wb") as file:  # TODO: Make this auto-generate file names using users' names
-                file.write(response)
-        cursor = get_db().cursor()
-        date = datetime.date.today().strftime("%Y-%M-%D")
-        time = datetime.datetime.now().strftime("%H:%M:%S")
-        cursor.execute(
-            "INSERT INTO equipment_log() VALUES($1, $2, $3, $4, $5) ",
-            (NotImplemented, date, time, NotImplemented, response),
-        )
+    #    if request.json is not None:
+    # response_encoded: str = request.json["dataUri"].split(",")[1]
+    # response = base64.b64decode(response_encoded)
+    # if True:  # TODO: Check if signature already exists before making it again
+    #     with open("response.svg", "wb") as file:  # TODO: Make this auto-generate file names using users' names
+    #         file.write(response)
+    # cursor = get_db().cursor()
+    # date = datetime.date.today().strftime("%Y-%M-%D")
+    # time = datetime.datetime.now().strftime("%H:%M:%S")
+    # cursor.execute(
+    #     "INSERT INTO equipment_log() VALUES($1, $2, $3, $4, $5) ",
+    #     (NotImplemented, date, time, NotImplemented, response),
+    # )
     return render_template("index.jinja")
 
 
@@ -50,3 +50,8 @@ def index():
 def js_serve():
     with open("templates/main.js") as file:
         return file.read()
+
+
+@app.route("/success", methods=["POST"])
+def success():
+    return render_template("success.jinja")
